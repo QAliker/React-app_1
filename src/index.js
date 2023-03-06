aa8b6905321551173e9c06df993eb2793d063e6e
Quentin <alikerquentin0@gmail.com>
2023-03-06 10:50:00 +0100

    navbar nearly finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import App from './app'
+import  './index.css'
 //hook de index.html
 ReactDOM.render(<App />, document.getElementById('root'))
